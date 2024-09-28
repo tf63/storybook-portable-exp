b06bd87a5dfa5fb6ffcd2a29bd8166199ee0012b
tf63 <74246282+tf63@users.noreply.github.com>
2024-09-29 02:02:01 +0900

    chore: clean up unnecessary files

--- packages/ui/src/components/not-violation-card/not-violation-card.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-
-import { NotViolationCard } from '.'
-
-test('renders NotViolationCard component', () => {
-    render(<NotViolationCard text="Not Violation" />)
-})
